8e18e958170c4e73511202ccd0244d37830870b7
Joesta Sebolela <joestax@icloud.com>
2025-08-29 00:53:47 +0200

    first commit

---
+++ android/app/src/main/kotlin/co/za/falcorp/zodiac_sky_finder/zodiac_sky_finder/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.za.falcorp.zodiac_sky_finder.zodiac_sky_finder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/za/co/falcorp/zaodiac_sign/zodiac_sky_finder/MainActivity.kt
@@ -0,0 +1,5 @@
+package za.co.falcorp.zaodiac_sign.zodiac_sky_finder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
